9ffe87b32c9de4248621d4f09c7610822cfd5ab9
potaesm <suthinan.musitmani@gmail.com>
2025-02-10 21:21:42 +0700

    update pkgs

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import pytest
 
 if __name__ == "__main__":
     # Run pytest on the tests directory
-    exit_code = pytest.main(["-v", "./tests"])
+    exit_code = pytest.main(["-v", "./tests/test_models.py"])
 
     sys.exit(exit_code)
